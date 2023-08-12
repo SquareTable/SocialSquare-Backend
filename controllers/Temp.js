13718ae9a02f8a3c8b96d20a1c94d743d0b11a79
Sebastian Webster <coolman16738@gmail.com>
2023-08-12 12:09:40 +1200

    fixed issue with reporting posts failing

--- controllers/Temp.js
+++ controllers/Temp.js
@@ -6602,7 +6602,7 @@ class TempController {
     }
 
     static reportPost = async (reporterId, postId, postFormat, reason) => {
-        return await this.#reportPost(reporterId, postId, postFormat, reasom)
+        return await this.#reportPost(reporterId, postId, postFormat, reason)
     }
 
     static userAlgorithmSettings = async (userId) => {
