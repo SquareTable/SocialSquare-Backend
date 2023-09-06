0c74a45711cb588402da0032270cfd0c0205d984
Sebastian Webster <coolman16738@gmail.com>
2023-09-07 01:02:29 +1200

    add dateJoined and role fields to CategoryMember model

--- models/CategoryMember.js
+++ models/CategoryMember.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const CategoryMemberSchema = new Schema({
     userId: mongoose.Schema.Types.ObjectId,
-    categoryId: mongoose.Schema.Types.ObjectId
+    categoryId: mongoose.Schema.Types.ObjectId,
+    dateJoined: Number,
+    role: String
 })
 
 const CategoryMemberModel = mongoose.model('CategoryMember', CategoryMemberSchema)
