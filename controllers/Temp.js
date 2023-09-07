23f293477f0019ee0004750d85d0c627b3a8fff1
Sebastian Webster <coolman16738@gmail.com>
2023-09-08 00:38:55 +1200

    Fixed geoIPLite not being defined

--- controllers/Temp.js
+++ controllers/Temp.js
@@ -40,6 +40,8 @@ const userHandler = new UserLibrary();
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
+const geoIPLite = require('geoip-lite')
+
 const { sendNotifications } = require("../notificationHandler");
 
 const { blurEmailFunction, mailTransporter } = require('../globalFunctions.js');
