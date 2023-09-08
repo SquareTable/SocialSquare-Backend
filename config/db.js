4d530c3fccf5895b8d03eb3de5f28247582f7bc9
Sebastian Webster <coolman16738@gmail.com>
2023-09-08 19:38:41 +1200

    remove useUnifiedTopology references

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 //require('dotenv').config();
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect(process.env.MONGODB_URI, {
-  useUnifiedTopology: true,
-})
+module.exports = mongoose.connect(process.env.MONGODB_URI)
