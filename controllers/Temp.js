0f8e8da80d2247da816e67cd9728f980cceebfc7
Sebastian Webster <coolman16738@gmail.com>
2023-10-12 16:34:16 +1300

    temp/pollpostcommentreply now returns isOwner

--- controllers/Temp.js
+++ controllers/Temp.js
@@ -777,6 +777,7 @@ class TempController {
                                 Poll.findOneAndUpdate({_id: {$eq: postId}}, { $push: { [`comments.${sentIndex}.commentReplies`]: commentForPost } }).then(function(){
                                     commentForPost.commentId = String(commentForPost.commentId)
                                     commentForPost.commenterId = String(commentForPost.commenterId)
+                                    commentForPost.isOwner = true;
                                     return resolve(HTTPWTHandler.OK('Comment upload successful', commentForPost))
                                 })
                                 .catch(err => {
