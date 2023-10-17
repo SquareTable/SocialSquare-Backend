468f02bab96f28628183d027f39179903bc32318
Sebastian Webster <coolman16738@gmail.com>
2023-10-18 01:26:45 +1300

    CommentCollection OTEC now unsets comments field

--- OneTimeExecuteCode/CommentCollection.js
+++ OneTimeExecuteCode/CommentCollection.js
@@ -1,9 +1,10 @@
 //This is the OneTimeExecuteCode for moving comments from being an array inside of a post document to being its own collection
 
 const ImagePost = require('../models/ImagePost');
-const Poll = require('../models/Poll')
-const Thread = require('../models/Thread')
-const Comment = require('../models/Comment')
+const Poll = require('../models/Poll');
+const Thread = require('../models/Thread');
+const Comment = require('../models/Comment');
+const mongoose = require('mongoose');
 
 //Usually we would have to implement functionality for comment replies, but as of writing this code, no comment replies have been made.
 //For the sake of simplicity, comment reply transfers will be ignored (since there are none)
@@ -54,10 +55,35 @@ Promise.all(
         }
     }
 
-    Comment.insertMany(updates).then(() => {
-        console.log('Successfully transferred', updates.length, 'comments into the Comment collection!')
+    mongoose.startSession().then(session => {
+        session.startTransaction();
+
+        Promise.all(
+            Comment.insertMany(updates, {session}),
+            ImagePost.findOneAndUpdate({"comments.0": {$exists: true}}, {$unset: {comments: ""}}, {session}),
+            Poll.findOneAndUpdate({"comments.0": {$exists: true}}, {$unset: {comments: ""}}, {session}),
+            Thread.findOneAndUpdate({"comments.0": {$exists: true}}, {$unset: {comments: ""}}, {session})
+        ).then(() => {
+            session.commitTransaction().then(() => {
+                session.endSession().then(() => {
+                    console.log('Successfully transferred', updates.length, 'comments into the Comment collection!')
+                }).catch(error => {
+                    console.error('An erorr occurred while ending Mongoose session:', error)
+                })
+            }).catch(error => {
+                console.error('An error occurred while committing comment transfer transaction:', error)
+                session.endSession().catch(error => {
+                    console.error('An error occurred while ending Mongoose session:', error)
+                })
+            })
+        }).catch(error => {
+            console.error('An error occurred while making comment transfer database operations:', error)
+            session.endSession().catch(error => {
+                console.error('An error occurred while ending Mongoose session:', error)
+            })
+        })
     }).catch(error => {
-        console.error('Transfer error occurred:', error)
+        console.error('An error occurred while starting Mongoose session:', error)
     })
 }).catch(error => {
     console.error('An error occurred while getting images, polls, and threads:', error)
