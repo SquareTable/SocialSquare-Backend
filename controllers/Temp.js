9f93ab15530891287af5ed0ff9e03b3ecbd62ab4
Sebastian Webster <coolman16738@gmail.com>
2023-10-21 04:16:33 +1300

    Added softDelete property when deleting already deleted comment

--- controllers/Temp.js
+++ controllers/Temp.js
@@ -4980,7 +4980,7 @@ class TempController {
                 if (!userFound) return resolve(HTTPWTHandler.notFound('Could not find user with provided userId'))
 
                 Comment.findOne({_id: {$eq: commentId}}).lean().then(commentFound => {
-                    if (!commentFound) return resolve(HTTPWTHandler.OK('Comment is already deleted.'))
+                    if (!commentFound) return resolve(HTTPWTHandler.OK('Comment is already deleted.', {softDelete: false}))
                     if (String(commentFound.commenterId) !== userId) return resolve(HTTPWTHandler.unauthorized('You are not allowed to delete this comment.'))
 
                     if (!commentFound.parentCommentId) {
