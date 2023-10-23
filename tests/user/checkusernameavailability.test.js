d6ce49548faee29bd30683dc9f3a5a9a082eb510
Sebastian Webster <coolman16738@gmail.com>
2023-10-24 04:16:42 +1300

    Updated test

--- tests/user/checkusernameavailability.test.js
+++ tests/user/checkusernameavailability.test.js
@@ -9,9 +9,9 @@ test('user/checkusernameavailability says available when username is available',
     DB.startServer().then(async uri => {
         await mongoose.connect(uri);
 
-        UserController.checkusernameavailability('seb').then(data => {
+        UserController.checkusernameavailability('seb').then(returned => {
             DB.stopServer().then(() => {
-                expect(data.message).toBe('Username is available')
+                expect(returned.data.message).toBe('Username is available')
             })
         })
     })
