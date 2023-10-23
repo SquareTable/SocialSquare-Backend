a309a6de94c8536ebeedab47afcb0b09ca6a0d44
Sebastian Webster <coolman16738@gmail.com>
2023-10-24 06:47:48 +1300

    Feed.js routes file is complete

--- routes/Feed.js
+++ routes/Feed.js
@@ -11,6 +11,7 @@ router.all("*", [tokenValidation]); // the * just makes it that it affects them 
 const workerPath = path.resolve('workers', 'FeedWorker.js')
 
 router.post('/viewedPostInFeed', (req, res) => {
+    let HTTPHeadersSent = false;
     const worker = new Worker(workerPath, {
         workerData: {
             functionName: 'viewedPostInFeed',
@@ -19,16 +20,25 @@ router.post('/viewedPostInFeed', (req, res) => {
     })
 
     worker.on('message', (result) => {
-        res.status(result.statusCode).json(result.data)
+        if (!HTTPHeadersSent) {
+            res.status(result.statusCode).json(result.data)
+        } else {
+            console.error('POST feed/viewedPostInFeed controller function returned data to be sent to the client but HTTP headers have already been sent! Data to be returned:', result)
+        }
     })
 
     worker.on('error', (error) => {
-        console.error('An error occurred from FeedWorker for POST /viewedPostInFeed:', error)
-        HTTPHandler.serverError(res, 'A server error occurred. Please try again.')
+        if (!HTTPHeadersSent) {
+            console.error('An error occurred from FeedWorker for POST /viewedPostInFeed:', error)
+            HTTPHandler.serverError(res, 'A server error occurred. Please try again.')
+        } else {
+            console.error('POST feed/viewedPostInFeed controller function encountered an error and tried to return it to the client but HTTP headers have already been sent! Error to be returned:', error)
+        }
     })
 })
 
 router.post('/followerFeed', (req, res) => {
+    let HTTPHeadersSent = false;
     const worker = new Worker(workerPath, {
         workerData: {
             functionName: 'followerFeed',
@@ -37,16 +47,25 @@ router.post('/followerFeed', (req, res) => {
     })
 
     worker.on('message', (result) => {
-        res.status(result.statusCode).json(result.data)
+        if (!HTTPHeadersSent) {
+            res.status(result.statusCode).json(result.data)
+        } else {
+            console.error('POST feed/followerFeed controller function returned data to be sent to the client but HTTP headers have already been sent! Data to be returned:', result)
+        }
     })
 
     worker.on('error', (error) => {
-        console.error('An error occurred from FeedWorker for POST /followerFeed:', error)
-        HTTPHandler.serverError(res, 'A server error occurred. Please try again.')
+        if (!HTTPHeadersSent) {
+            console.error('An error occurred from FeedWorker for POST /followerFeed:', error)
+            HTTPHandler.serverError(res, 'A server error occurred. Please try again.')
+        } else {
+            console.error('POST feed/followerFeed controller function encountered an error and tried to return it to the client but HTTP headers have already been sent! Error to be returned:', error)
+        }
     })
 })
 
 router.post('/forYouFeed', (req, res) => {
+    let HTTPHeadersSent = false;
     const worker = new Worker(workerPath, {
         workerData: {
             functionName: 'forYouFeed',
@@ -55,12 +74,20 @@ router.post('/forYouFeed', (req, res) => {
     })
 
     worker.on('message', (result) => {
-        res.status(result.statusCode).json(result.data)
+        if (!HTTPHeadersSent) {
+            res.status(result.statusCode).json(result.data)
+        } else {
+            console.error('POST feed/forYouFeed controller function returned data to be sent to the client but HTTP headers have already been sent! Data to be returned:', result)
+        }
     })
 
     worker.on('error', (error) => {
-        console.error('An error occurred from FeedWorker for POST /forYouFeed:', error)
-        HTTPHandler.serverError(res, 'A server error occurred. Please try again.')
+        if (!HTTPHeadersSent) {
+            console.error('An error occurred from FeedWorker for POST /forYouFeed:', error)
+            HTTPHandler.serverError(res, 'A server error occurred. Please try again.')
+        } else {
+            console.error('POST feed/forYouFeed controller function encountered an error and tried to return it to the client but HTTP headers have already been sent! Error to be returned:', error)
+        }
     })
 })
 
