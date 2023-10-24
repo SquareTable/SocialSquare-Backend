8608e8170d55ddfecbc0d29f225c1c251a58c293
Sebastian Webster <coolman16738@gmail.com>
2023-10-24 21:26:26 +1300

    Add another test case for user/checkusernameavailability

--- tests/user/checkusernameavailability.test.js
+++ tests/user/checkusernameavailability.test.js
@@ -36,3 +36,20 @@ test('user/checkusernameavailability says not available when username is not ava
 
     expect(returned.data.message).toBe('Username is not available')
 })
+
+test('user/checkusernameavailability says not available when uppercased username is not available', async () => {
+    const DB = new MockMongoDBServer()
+    const uri = await DB.startServer();
+
+    await mongoose.connect(uri);
+    
+    const newUser = new User({name: 'SEB'});
+    await newUser.save();
+
+    const returned = await UserController.checkusernameavailability('seb')
+
+    await mongoose.disconnect();
+    await DB.stopServer();
+
+    expect(returned.data.message).toBe('Username is not available')
+})
