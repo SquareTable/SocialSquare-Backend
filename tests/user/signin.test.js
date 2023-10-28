c7a0454467a59fc99db454fb878c58ed61cd47e8
Sebastian-Webster <coolman16738@gmail.com>
2023-10-29 08:49:18 +1300

    Add test file

---
+++ tests/user/signin.test.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const MockMongoDBServer = require('../../libraries/MockDBServer');
+
+const User = require('../../models/User');
+const RefreshToken = require('../../models/RefreshToken');
+
+const {expect} = require('@jest/globals');
+
+/*
+TODO:
+
+*/
