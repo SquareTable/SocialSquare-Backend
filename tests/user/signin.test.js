c1e15fd9c36a817d684f1cd1e9572ad80bdd0ebc
Sebastian-Webster <coolman16738@gmail.com>
2023-10-30 09:08:40 +1300

    Added more tests for user/signin

--- tests/user/signin.test.js
+++ tests/user/signin.test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const MockMongoDBServer = require('../../libraries/MockDBServer');
 
 const User = require('../../models/User');
@@ -16,12 +17,12 @@ const validDeviceName = "GitHub-Actions"
 
 /*
 TODO:
-Test if signin fails if email is not a string
-Test if signin fails if password is not a string
-Test if signin fails if email is an empty string
-Test if signin fails if password is an empty string
-Test if signin fails if user with specified email could not be found
-Test if signin fails if password is wrong (testing if user/signin carries out correct authentication)
+Test if signin fails if email is not a string -- Done
+Test if signin fails if password is not a string -- Done
+Test if signin fails if email is an empty string -- Done
+Test if signin fails if password is an empty string -- Done
+Test if signin fails if user with specified email could not be found -- Done
+Test if signin fails if password is wrong (testing if user/signin carries out correct authentication) -- Done
 
 Tests if email 2FA is enabled:
     - Test if email gets blurred
@@ -47,11 +48,85 @@ Tests if email 2FA is not enabled:
     - Test if already existing User documents do not get modified
 */
 
-for (const invalidEmail of TEST_CONSTANTS.NOT_STRINGS) {
-    test(`If signin fails if email is not a string. Testing: ${JSON.stringify(invalidEmail)}`, async () => {
-        const returned = await UserController.signin(invalidEmail, validPassword, validIP, validDeviceName);
+for (const notString of TEST_CONSTANTS.NOT_STRINGS) {
+    test(`If signin fails if email is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+        expect.assertions(2);
+
+        const returned = await UserController.signin(notString, validPassword, validIP, validDeviceName);
 
         expect(returned.statusCode).toBe(400);
-        expect(returned.data.message).toBe(`email must be a string. Provided type: ${typeof invalidEmail}`)
+        expect(returned.data.message).toBe(`email must be a string. Provided type: ${typeof notString}`)
+    })
+
+    test(`If signin fails if password is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+        expect.assertions(2);
+
+        const returned = await UserController.signin(validEmail, notString, validIP, validDeviceName);
+
+        expect(returned.statusCode).toBe(400);
+        expect(returned.data.message).toBe(`password must be a string. Provided type: ${typeof notString}`)
     })
 }
+
+test(`If signin fails if email is an empty string`, async () => {
+    expect.assertions(2);
+
+    const returned = await UserController.signin('', validPassword, validIP, validDeviceName);
+
+    expect(returned.statusCode).toBe(400);
+    expect(returned.data.message).toBe('Email cannot be blank')
+})
+
+test('If signin fails if password is an empty string', async () => {
+    expect.assertions(2);
+
+    const returned = await UserController.signin(validEmail, '', validIP, validDeviceName);
+
+    expect(returned.statusCode).toBe(400);
+    expect(returned.data.message).toBe("Password cannot be blank")
+})
+
+test('If signin fails if user with specified email could not be found', async () => {
+    expect.assertions(2);
+
+    const DB = new MockMongoDBServer();
+    const uri = await DB.startServer();
+
+    await mongoose.connect(uri);
+
+    const returned = await UserController.signin(validEmail, validPassword, validIP, validDeviceName);
+
+    await mongoose.disconnect();
+    await DB.stopServer();
+
+    expect(returned.statusCode).toBe(404);
+    expect(returned.data.message).toBe("A user with the specified email does not exist.")
+})
+
+test('If signin fails if password is wrong', async () => {
+    expect.assertions(2);
+
+    const DB = new MockMongoDBServer();
+    const uri = await DB.startServer();
+
+    await mongoose.connect(uri);
+
+    const userData = {
+        email: validEmail,
+        password: bcrypt.hashSync(validPassword, 12)
+    }
+
+    await new User(userData).save();
+
+    const returned = await UserController.signin(userData.email, 'wrongpassword', validIP, validDeviceName);
+
+    await mongoose.disconnect();
+    await DB.stopServer();
+
+    expect(returned.statusCode).toBe(401);
+    expect(returned.data.message).toBe("Invalid password entered!");
+    expect(returned.data.data).toBe(undefined);
+    expect(returned.data.token).toBe(undefined);
+    expect(returned.data.refreshToken).toBe(undefined);
+    expect(returned.data.refreshTokenId).toBe(undefined);
+})
