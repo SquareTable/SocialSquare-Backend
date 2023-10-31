e10d9ca5da0a05f13672e2dbc87b7cf759f1e085
Sebastian Webster <coolman16738@gmail.com>
2023-11-01 10:34:11 +1300

    Added more temp/changeemail tests

--- tests/temp/changeemail.test.js
+++ tests/temp/changeemail.test.js
@@ -31,15 +31,15 @@ const userData = {
 
 /*
 TODO:
-Test if change fails if userId is not a string
-Test if change fails if userId is not an objectId
-Test if change fails if password is not a string
-Test if change fails if password is an empty string
-Test if change fails if desiredEmail is not a string
-Test if change fails if desiredEmail is an empty string
-Test if change fails if desiredEmail does not pass the valid email test
-Test if change fails if user with userId could not be found
-Test if change fails if there is already a user with the desired email
+Test if change fails if userId is not a string -- Done
+Test if change fails if userId is not an objectId -- Done
+Test if change fails if password is not a string -- Done
+Test if change fails if password is an empty string -- Done
+Test if change fails if desiredEmail is not a string -- Done
+Test if change fails if desiredEmail is an empty string -- Done
+Test if change fails if desiredEmail does not pass the valid email test -- Done
+Test if change fails if user with userId could not be found -- Done
+Test if change fails if there is already a user with the desired email -- Done
 test if change fails if password is wrong
 Test if change is successful when inputs are correct
 Test if change does not modify other User documents
@@ -56,4 +56,101 @@ for (const notString of TEST_CONSTANTS.NOT_STRINGS) {
         expect(returned.statusCode).toBe(400);
         expect(returned.data.message).toBe(`userId must be a string. Type provided: ${typeof notString}`)
     })
+
+    test(`If change fails if password is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+        expect.assertions(2);
+
+        await new User(userData).save();
+
+        const returned = await TempController.changeemail(String(userData._id), notString, validEmail);
+
+        expect(returned.statusCode).toBe(400);
+        expect(returned.data.message).toBe(`password must be a string. Provided type: ${typeof notString}`)
+    })
+
+    test(`If change fails if desiredEmail is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+        expect.assertions(2);
+
+        await new User(userData).save();
+
+        const returned = await TempController.changeemail(String(userData._id), validPassword, notString);
+
+        expect(returned.statusCode).toBe(400);
+        expect(returned.data.message).toBe(`desiredEmail must be a string. Provided type: ${typeof notString}`)
+    })
 }
+
+test('If change fails if userId is not an objectId', async () => {
+    expect.assertions(2);
+
+    await new User(userData).save();
+
+    const returned = await TempController.changeemail('i am not an objectId', validPassword, validEmail);
+
+    expect(returned.statusCode).toBe(400);
+    expect(returned.data.message).toBe('userId must be an objectId.')
+})
+
+test('If change fails if password is an empty string', async () => {
+    expect.assertions(2);
+
+    await new User(userData).save();
+
+    const returned = await TempController.changeemail(String(userData._id), '', validEmail);
+
+    expect(returned.statusCode).toBe(400);
+    expect(returned.data.message).toBe('Password cannot be blank.')
+})
+
+test('if change fails if desiredEmail is an empty string', async () => {
+    expect.assertions(2);
+
+    await new User(userData).save();
+
+    const returned = await TempController.changeemail(String(userData._id), validPassword, '');
+    
+    expect(returned.statusCode).toBe(400);
+    expect(returned.data.message).toBe('Desired email cannot be blank.')
+})
+
+for (const invalidEmail of TEST_CONSTANTS.INVALID_EMAILS) {
+    test(`If change fails if desiredEmail does not pass the email validity test. Testing: ${invalidEmail}`, async () => {
+        expect.assertions(2);
+
+        await new User(userData).save();
+
+        const returned = await TempController.changeemail(String(userData._id), validPassword, invalidEmail);
+
+        expect(returned.statusCode).toBe(400);
+        expect(returned.data.message).toBe('Invalid desired email entered')
+    })
+}
+
+test('If change fails if user with userId cannot be found', async () => {
+    expect.assertions(2);
+
+    const returned = await TempController.changeemail(String(userData._id), validPassword, validEmail)
+
+    expect(returned.statusCode).toBe(404);
+    expect(returned.data.message).toBe('Could not find user with provided userId.')
+})
+
+test('If change fails if user with desired email already exists', async () => {
+    expect.assertions(2);
+
+    const newUserData = {
+        _id: new mongoose.Types.ObjectId(),
+        secondId: uuidv4(),
+        name: 'sebastian2',
+        displayName: 'SebastianTwo',
+        email: 'alreadytaken@gmail.com'
+    }
+
+    await new User(userData).save();
+    await new User(newUserData).save();
+
+    const returned = await TempController.changeemail(String(userData._id), validPassword, newUserData.email);
+
+    expect(returned.statusCode).toBe(403);
+    expect(returned.data.message).toBe('User with the desired email already exists')
+})
