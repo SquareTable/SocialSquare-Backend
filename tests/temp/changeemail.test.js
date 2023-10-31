fcefbed3fbb520a5b7d0a9a0a3c3a1218862c81b
Sebastian-Webster <coolman16738@gmail.com>
2023-11-01 09:16:59 +1300

    Added userId not string test

--- tests/temp/changeemail.test.js
+++ tests/temp/changeemail.test.js
@@ -2,6 +2,32 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const TempController = require('../../controllers/Temp');
 const User = require('../../models/User');
+const TEST_CONSTANTS = require('../TEST_CONSTANTS');
+const {expect, test, beforeEach, afterEach} = require('@jest/globals');
+const {v4: uuidv4} = require('uuid');
+
+const DB = new MockMongoDBServer();
+
+beforeEach(async () => {
+    await DB.startTest();
+})
+
+afterEach(async () => {
+    await DB.stopTest();
+})
+
+const validEmail = 'john.sullivan@gmail.com';
+const validPassword = 'mypassword';
+const validHashedPassword = '$2y$10$lIUHn3yo0uF05O.RA38QSOdqF270MQboayDZHifGm/jN1iJy.Tvna';
+
+const userData = {
+    _id: new mongoose.Types.ObjectId(),
+    secondId: uuidv4(),
+    name: 'sebastian',
+    displayName: 'Sebastian',
+    email: 'myemail@gmail.com',
+    password: validHashedPassword
+}
 
 /*
 TODO:
@@ -18,3 +44,16 @@ test if change fails if password is wrong
 Test if change is successful when inputs are correct
 Test if change does not modify other User documents
 */
+
+for (const notString of TEST_CONSTANTS.NOT_STRINGS) {
+    test(`If change fails if userId is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+        expect.assertions(2);
+
+        await new User(userData).save();
+
+        const returned = await TempController.changeemail(notString, validPassword, validEmail);
+        
+        expect(returned.statusCode).toBe(400);
+        expect(returned.data.message).toBe(`userId must be a string. Type provided: ${typeof notString}`)
+    })
+}
