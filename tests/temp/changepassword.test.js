c7df5e18a4c7dc1cc39c0207c00e8028af9844f2
Sebastian Webster <coolman16738@gmail.com>
2023-11-01 11:06:54 +1300

    Added temp/changepassword test

---
+++ tests/temp/changepassword.test.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {v4: uuidv4} = require('uuid');
+
+const User = require('../../models/User');
+const RefreshToken = require('../../models/RefreshToken');
+const MockMongoDBServer = require('../../libraries/MockDBServer');
+const TEST_CONSTANTS = require('../TEST_CONSTANTS');
+const TempController = require('../../controllers/Temp');
+
+const {expect, beforeEach, afterEach, test} = require('@jest/globals');
+
+const DB = new MockMongoDBServer();
+
+beforeEach(async () => {
+    await DB.startTest();
+})
+
+afterEach(async () => {
+    await DB.stopTest();
+})
+
+const validEmail = "john.sullivan@gmail.com";
+const validPassword = "securepassword";
+const validIP = "127.0.0.1";
+const validDeviceName = "GitHub-Actions";
+
+const newPassword = 'mynewpassword';
+
+const validHashedPassword = "$2b$10$34gQut./qmo7HoG1aKkQeOQWeZzCjwwMMgk8nsLpwb3snlKK0wRKy";
+
+const userData = {
+    _id: new mongoose.Types.ObjectId(),
+    secondId: uuidv4(),
+    email: validEmail,
+    password: validHashedPassword
+}
+
+/*
+TODO:
+Test if change fails if userId is not a string
+Test if change fails if userId is not an objectId
+Test if change fails if currentPassword is not a string
+Test if change fails if newPassword is not a string
+Test if change fails if confirmNewPassword is not a string
+Test if change fails if newPassword is less than 8 characters
+Test if change fails if newPassword is more than 17 characters
+Test if change fails if newPassword and confirmNewPassword are not the same
+Test if change fails if user with userId cannot be found
+Test if change fails if currentPassword is wrong
+Test if new token is generated and it is usable
+Test if new refreshToken is generated and it is usable
+Test if new encryptedRefreshToken is generated and it can be decrypted back to refreshToken
+Test RefreshToken document is created with admin set to false
+Test if IP is saved to RefreshToken document only if user allows it
+Test if IP is not saved to RefreshToken document if user does not allow it
+Test if IP-derived location is saved to RefreshToken document only if user allows it
+Test if IP-derived location is not saved to RefreshToken document if user does not allow it
+Test if IP-derived location is set to "Unknown Location" if the location cannot be found
+Test if deviceType is saved to the RefreshToken document only if the user allows it
+Test if deviceType is not saved to the RefreshToken document if the user does not allow it
+Test if password change is successful with correct inputs
+Test if all previous RefreshTokens from the same user are removed when password is changed
+*/
+
+for (const notString of TEST_CONSTANTS.NOT_STRINGS) {
+    test(`If change fails if userId is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+        expect.assertions(2);
+
+        const returned = await TempController.changepassword(notString, validPassword, newPassword, newPassword, validIP, validDeviceName);
+
+        expect(returned.statusCode).toBe(400);
+        expect(returned.data.message).toBe(`userId must be a string. Type provided: ${typeof notString}`)
+    })
+}
