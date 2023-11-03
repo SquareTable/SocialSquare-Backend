708da901e37f531a797f12ff4fdc76e5dc902a74
Sebastian Webster <coolman16738@gmail.com>
2023-11-04 11:33:56 +1300

    Changed message

--- controllers/Temp.js
+++ controllers/Temp.js
@@ -347,28 +347,25 @@ class TempController {
             }
         
             User.findOne({_id: {$eq: userId}}).lean().then(userFound => {
-                if (userFound) {
-                    //User Exists
-                    User.findOne({name: {$eq: desiredUsername}}).lean().then(result => {
-                        // A username exists
-                        if (result) {
-                            return resolve(HTTPWTHandler.conflict('User with the provided username already exists'))
-                        } else {
-                            User.findOneAndUpdate({_id: {$eq: userId}}, {name: String(desiredUsername)}).then(function(){
-                                return resolve(HTTPWTHandler.OK('Change Username Successful'))
-                            })
-                            .catch(err => {
-                                console.error('An error occured while updating user with id:', userId, ' to have a username:', desiredUsername, '. The error was:', err)
-                                return resolve(HTTPWTHandler.serverError('An error occurred while updating your username. Please try again.'))
-                            });
-                        }
-                    }).catch(error => {
-                        console.error('An error occured while finding one user with name:', desiredUsername, '. The error was:', error)
-                        return resolve(HTTPWTHandler.serverError('An error occurred while checking for existing user. Please try again.'))
-                    })
-                } else {
-                    return resolve(HTTPWTHandler.notFound('User with provided userId could not be found'))
-                }
+                if (!userFound) return resolve(HTTPWTHandler.notFound('Could not find user with provided userId.'));
+
+                User.findOne({name: {$eq: desiredUsername}}).lean().then(result => {
+                    // A username exists
+                    if (result) {
+                        return resolve(HTTPWTHandler.conflict('User with the provided username already exists'))
+                    } else {
+                        User.findOneAndUpdate({_id: {$eq: userId}}, {name: String(desiredUsername)}).then(function(){
+                            return resolve(HTTPWTHandler.OK('Change Username Successful'))
+                        })
+                        .catch(err => {
+                            console.error('An error occured while updating user with id:', userId, ' to have a username:', desiredUsername, '. The error was:', err)
+                            return resolve(HTTPWTHandler.serverError('An error occurred while updating your username. Please try again.'))
+                        });
+                    }
+                }).catch(error => {
+                    console.error('An error occured while finding one user with name:', desiredUsername, '. The error was:', error)
+                    return resolve(HTTPWTHandler.serverError('An error occurred while checking for existing user. Please try again.'))
+                })
             }).catch(err => {
                 console.error('An error occured while checking for a user with id:', userId, '. The error was:', err)
                 return resolve(HTTPWTHandler.serverError('An error occurred while finding user. Please try again.'))
