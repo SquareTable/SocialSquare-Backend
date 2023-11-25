7689bcefc1bdfc1b9eec33ddc9af009a224d6b78
Sebastian Webster <coolman16738@gmail.com>
2023-11-25 19:45:25 +1300

    Added temp/deletenotification type tests

--- tests/temp/deletenotification.test.js
+++ tests/temp/deletenotification.test.js
@@ -27,9 +27,9 @@ const notificationData = {
 /*
 TODO:
 - Test if deletion fails if userId is not a string -- Done
-- Test if deletion fails if userId is not an ObjectId
-- Test if deletion fails if notificationId is not a string
-- Test if deletion fails if notificationId is not an ObjectId
+- Test if deletion fails if userId is not an ObjectId -- Done
+- Test if deletion fails if notificationId is not a string -- Done
+- Test if deletion fails if notificationId is not an ObjectId -- Done
 - Test if deletion fails if user could not be found
 - Test if deletion fails if notification could not be found
 - Test if deletion fails if the user is not the notification owner
@@ -46,4 +46,31 @@ for (const notString of TEST_CONSTANTS.NOT_STRINGS) {
 		expect(returned.statusCode).toBe(400);
 		expect(returned.data.message).toBe(`userId must be a string. Provided type: ${typeof notString}`)
 	})
+
+	test(`If deletion fails if notificationId is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+		expect.assertions(2);
+
+		const returned = await TempController.deletenotification(userData._id, notString);
+
+		expect(returned.statusCode).toBe(400);
+		expect(returned.data.message).toBe('notificationId must be an ObjectId.')
+	})
 }
+
+test('If deletion fails if userId is not an ObjectId', async () => {
+	expect.assertions(2);
+
+	const returned = await TempController.deletenotification('i am not an ObjectId', notificationData._id);
+
+	expect(returned.statusCode).toBe(400);
+	expect(returned.data.message).toBe('userId must be an ObjectId.')
+})
+
+test('If deletion fails if notificationId is not an ObjectId', async () => {
+	expect.assertions(2);
+
+	const returned = await TempController.deletenotification(userData._id, 'i am not an ObjectId');
+
+	expect(returned.statusCode).toBe(400);
+	expect(returned.data.message).toBe('notificationId must be an ObjectId.')
+})
