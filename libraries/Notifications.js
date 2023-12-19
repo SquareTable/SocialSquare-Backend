5a8742f9067ce4006d3e3ad709026bea2e03509c
Sebastian Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2023-12-20 00:43:05 +1300

    Stringify notification id

--- libraries/Notifications.js
+++ libraries/Notifications.js
@@ -2,6 +2,7 @@ class Notifications {
     returnNotificationDataToSend(rawNotificationData) {
         return rawNotificationData.map(notification => {
             delete notification.userId;
+            notification._id = String(notification._id)
             return notification;
         })
     }
