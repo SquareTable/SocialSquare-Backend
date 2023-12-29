22795c98d8f15162a6d5df335344b24d62bf6334
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2023-12-30 03:56:36 +1300

    Added first temp/getnotifications test

--- tests/temp/getnotifications.test.js
+++ tests/temp/getnotifications.test.js
@@ -9,10 +9,31 @@ jest.setTimeout(20_000);
 
 /*
 TODO:
-- Test if notification retrieval fails if userId is not a string
+- Test if notification retrieval fails if userId is not a string -- Done
 - Test if notification retrieval fails if userId is not an ObjectId
 - Test if notification retrieval fails if lastNotificationId is not a string and not undefined
 - Test if notification retrieval fails if user could not be found
 - Test if notification retrieval works with lastNotificationId
 - Test if notification retrieval works with lastNotificationId as undefined
 */
+
+const DB = new MockMongoDBServer();
+
+beforeEach(async () => {
+    await DB.startTest()
+})
+
+afterEach(async () => {
+    await DB.stopTest()
+})
+
+for (const notString of TEST_CONSTANTS.NOT_STRINGS) {
+    test(`If retrieval fails if userId is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
+        expect.assertions(2);
+
+        const returned = await TempController.getnotifications(notString, undefined);
+
+        expect(returned.statusCode).toBe(400);
+        expect(returned.data.message).toBe(`userId must be a string. Provided type: ${typeof notString}`)
+    })
+}
