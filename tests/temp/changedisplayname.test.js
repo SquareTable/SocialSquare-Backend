5d4a558d7af6c638a7a112d7e57ba826c4730d8b
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-06-30 03:42:08 +1200

    Added all changes to temp/changedisplayname

--- tests/temp/changedisplayname.test.js
+++ tests/temp/changedisplayname.test.js
@@ -6,16 +6,20 @@ const mongoose = require('mongoose');
 
 const DB = new MockMongoDBServer();
 
-const {expect, beforeEach, afterEach} = require('@jest/globals');
+const {beforeAll, afterEach, afterAll, test, expect} = require('@jest/globals');
 
 jest.setTimeout(20_000) //20s per test
 
-beforeEach(async () => {
-    await DB.startTest();
+beforeAll(async () => {
+  await DB.startTest()
 })
 
 afterEach(async () => {
-    await DB.stopTest();
+  await DB.purgeData();
+})
+
+afterAll(async () => {
+  await DB.stopTest()
 })
 
 /*
@@ -39,63 +43,80 @@ const userData = {
 
 for (const notString of TEST_CONSTANTS.NOT_STRINGS) {
     test(`If change fails if userId is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
-        expect.assertions(2);
+        expect.assertions(3);
 
         await new User(userData).save();
+
+        await DB.takeDBSnapshot()
 
         const returned = await TempController.changedisplayname(notString, 'newname');
 
         expect(returned.statusCode).toBe(400);
         expect(returned.data.message).toBe(`userId must be a string. Provided type: ${typeof notString}`);
+        expect(await DB.noChangesMade()).toBe(true)
     })
 
     test(`If change fails if desiredDisplayName is not a string. Testing: ${JSON.stringify(notString)}`, async () => {
-        expect.assertions(2);
+        expect.assertions(3);
 
         await new User(userData).save();
+
+        await DB.takeDBSnapshot()
 
         const returned = await TempController.changedisplayname(String(userData._id), notString)
 
         expect(returned.statusCode).toBe(400);
         expect(returned.data.message).toBe(`desiredDisplayName must be a string. Provided type: ${typeof notString}`)
+        expect(await DB.noChangesMade()).toBe(true)
     })
 }
 
 test('If change fails if userId is not an objectId', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
 
     await new User(userData).save();
+
+    await DB.takeDBSnapshot()
 
     const returned = await TempController.changedisplayname('i am not an objectid', 'newname');
 
     expect(returned.statusCode).toBe(400);
     expect(returned.data.message).toBe('userId must be an objectId.')
+    expect(await DB.noChangesMade()).toBe(true)
 })
 
 test('If change fails if desiredDisplayName is longer than 20 characters', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
 
     await new User(userData).save();
+
+    await DB.takeDBSnapshot()
 
     const returned = await TempController.changedisplayname(String(userData._id), "this is 21 characters");
 
     expect(returned.statusCode).toBe(400);
     expect(returned.data.message).toBe('Desired display name must be 20 characters or less.')
+    expect(await DB.noChangesMade()).toBe(true)
 })
 
 test('If change fails if user could not be found', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
+
+    await DB.takeDBSnapshot()
 
     const returned = await TempController.changedisplayname(String(new mongoose.Types.ObjectId()), 'helloworld');
 
     expect(returned.statusCode).toBe(404);
     expect(returned.data.message).toBe('Could not find user with provided userId.')
+    expect(await DB.noChangesMade()).toBe(true)
 })
 
 test('If change successfully changes display name and only display name', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
 
     await new User(userData).save();
+
+    await DB.takeDBSnapshot()
 
     const beforeUser = await User.findOne({}).lean();
 
@@ -112,10 +133,11 @@ test('If change successfully changes display name and only display name', async 
     expect(returned.statusCode).toBe(200);
     expect(beforeUser).toStrictEqual(afterUser);
     expect(beforeDisplayName !== afterDisplayName).toBe(true);
+    expect(await DB.changedCollections()).toIncludeSameMembers(['User'])
 })
 
 test('If change does not modify already existing User documents', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
 
     const usersToInsert = [...new Array(10)].map((item, index) => {
         return {
@@ -130,32 +152,41 @@ test('If change does not modify already existing User documents', async () => {
 
     await new User(userData).save();
 
+    await DB.takeDBSnapshot()
+
     const returned = await TempController.changedisplayname(String(userData._id), 'newdisplayname');
 
     const afterUsers = await User.find({_id: {$ne: userData._id}}).lean();
 
     expect(returned.statusCode).toBe(200);
     expect(beforeUsers).toStrictEqual(afterUsers);
+    expect(await DB.changedCollections()).toIncludeSameMembers(['User'])
 })
 
 test('If change fails if desiredDisplayName is multi-line', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
 
     await new User(userData).save();
+
+    await DB.takeDBSnapshot()
 
     const returned = await TempController.changedisplayname(String(userData._id), `new\ndisplay\nname`);
 
     expect(returned.statusCode).toBe(400);
     expect(returned.data.message).toBe("Display name must only contain characters in the alphabet and must be a single line.")
+    expect(await DB.noChangesMade()).toBe(true)
 })
 
 test('If change fails if desiredDisplayName contains non-alphabetic characters', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
 
     await new User(userData).save();
+
+    await DB.takeDBSnapshot()
 
     const returned = await TempController.changedisplayname(String(userData._id), '*&^%$#%^&*()')
 
     expect(returned.statusCode).toBe(400);
     expect(returned.data.message).toBe("Display name must only contain characters in the alphabet and must be a single line.")
+    expect(await DB.noChangesMade()).toBe(true)
 })
