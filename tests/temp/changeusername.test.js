3afa9f52a23d9bf3fd9d3285f67bd62e08d42aa4
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-06-30 05:44:10 +1200

    Added temporary debug step

--- tests/temp/changeusername.test.js
+++ tests/temp/changeusername.test.js
@@ -175,7 +175,7 @@ test('If change is successful with correct inputs', async () => {
 test('that successful change of username does not interfere with other User documents', async () => {
     expect.assertions(3);
 
-    console.error('Users found:', await User.find({}))
+    throw new Error(`Users found: ${JSON.stringify(await User.find({}))}`)
 
     const users = [...new Array(10)].map((item, index) => {
         return {
