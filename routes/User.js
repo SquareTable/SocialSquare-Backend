d91629a044929de328752097ff969965bed7c61c
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-09-22 16:51:54 +1200

    revert user/checkusernameavailability changes

--- routes/User.js
+++ routes/User.js
@@ -116,7 +116,7 @@ router.post('/checkusernameavailability', rateLimiters['/checkusernameavailabili
     const worker = new Worker(workerPath, {
         workerData: {
             functionName: 'checkusernameavailability',
-            functionArgs: [req.body]
+            functionArgs: [req.body.username]
         }
     })
 
