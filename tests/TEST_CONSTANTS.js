d90df460632863c864538e3eb65d97af4faea9d7
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2025-03-21 06:26:22 +0400

    all user route tests now use supertest

--- tests/TEST_CONSTANTS.js
+++ tests/TEST_CONSTANTS.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const TEST_CONSTANTS = {
     NOT_STRINGS: [true, false, undefined, null, {}, [], 1, -1],
     INVALID_EMAILS: ["notanemail", "notanemail@gmail.notanemail"],
+    INVALID_NAMES: ["abc12!", "abc._.abc", "abc!@#$%^&*()", "(", ")", "$%^&*wegyf"],
+    VALID_EMAILS: ["john.sullivan@gmail.com", "john.sullivan@hotmail.com", "john.sullivan123@gmail.com", "mytestemail@gmail.com", "mytestemail@hotmail.com", "myyahooemail@yahoo.com"],
     JWTVerifier: (secret, token) => {
         return new Promise(resolve => {
             jwt.verify(token, secret, (err, decoded) => {
