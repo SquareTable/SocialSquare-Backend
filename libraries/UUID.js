e0554c6fc0a6edb80ead9dde61ab5490025ee59a
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2025-11-12 17:08:31 +0800

    check if uuid is a string

--- libraries/UUID.js
+++ libraries/UUID.js
@@ -1,5 +1,6 @@
 class UUIDLibrary {
     validateV4(uuid) {
+        if (typeof uuid !== 'string') return false
         if (uuid.length !== 36) return false
         return /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/.test(uuid)
     }
